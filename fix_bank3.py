41c2f6602e400c8533eed63d369dc8e12d2a9f61
askgogo84 <goverdhan.md@gmail.com>
2026-05-27 14:08:34 +0530

    chore: remove fix scripts from repo

--- fix_bank3.py
+++
@@ -1,9 +0,0 @@
-﻿with open('app/bank/[slug]/page.tsx', 'r', encoding='utf-8') as f:
-    s = f.read()
-s = s.replace(
-    '<CardTile key={card.id} card={toTileCard(card, i)} rank={i + 1} />',
-    '<CardTile key={card.id} card={toTileCard(card, i)} rank={i + 1} href={`/card/${card.slug}`} />'
-)
-with open('app/bank/[slug]/page.tsx', 'w', encoding='utf-8') as f:
-    f.write(s)
-print('Done')
--- fix_dup.py
+++
@@ -1,7 +0,0 @@
-﻿with open('app/card-roast/page.tsx', 'r', encoding='utf-8') as f:
-    s = f.read()
-s = s.replace(' className="grid-1-mobile" className="grid-1-mobile"', ' className="grid-1-mobile"')
-s = s.replace(' className="grid-2-mobile" className="grid-2-mobile"', ' className="grid-2-mobile"')
-with open('app/card-roast/page.tsx', 'w', encoding='utf-8') as f:
-    f.write(s)
-print('Done')
